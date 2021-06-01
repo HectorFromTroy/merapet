18da0f67190b9e437ada0a9c03b2987b81ae6973
Niyaz Nazipov <niaz.nazipoff@yandex.ru>
2021-06-01 11:16:29 +0300

    added uuid generator for users

--- src/main/java/com/nazipov/merapet/storage/Storage.java
+++ src/main/java/com/nazipov/merapet/storage/Storage.java
@@ -1,10 +1,22 @@
 package com.nazipov.merapet.storage;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
 
 import com.nazipov.merapet.entities.MyUser;
 
 public class Storage {
+    // String - user id
     private static HashMap<String, MyUser> users = new HashMap<>();
-    
+    // Key - user id, value - set of contact id's
+    private static HashMap<String, HashSet<String>> contacts = new HashMap<>();
+
+    public String getUUID() {
+        String uuid = UUID.randomUUID().toString();
+        while (users.containsKey(uuid)) {
+            uuid = UUID.randomUUID().toString();
+        }
+        return uuid;
+    }
 }
