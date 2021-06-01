8bf54e880a7f2a337979ee2d6e5250f8b184a358
Niyaz Nazipov <niaz.nazipoff@yandex.ru>
2021-06-01 10:35:32 +0300

    implemented MyUser entity

--- src/main/java/com/nazipov/merapet/entities/MyUser.java
+++ src/main/java/com/nazipov/merapet/entities/MyUser.java
@@ -10,4 +10,82 @@ public class MyUser {
     private final String email;
     private final LocalDateTime dateOfBirth;
     private final Gender gender;
+
+    public MyUser(
+        String username,
+        String password,
+        String email,
+        LocalDateTime dateOfBirth,
+        Gender gender
+    ) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+
+        private String username;
+        private String password;
+        private String email;
+        private LocalDateTime dateOfBirth;
+        private Gender gender;
+
+        private Builder() {}
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setDateOfBirth(LocalDateTime dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public Builder setGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public MyUser build() {
+            return new MyUser(username, password, email, dateOfBirth, gender);
+        }
+    }
 }
