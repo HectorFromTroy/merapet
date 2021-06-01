0f62191d8578f85d52e36ae2fc469682bdef4be8
Niyaz Nazipov <niaz.nazipoff@yandex.ru>
2021-06-01 10:47:40 +0300

    implemented user info dto

--- src/main/java/com/nazipov/merapet/dto/UserInfo.java
+++ src/main/java/com/nazipov/merapet/dto/UserInfo.java
@@ -1,5 +1,72 @@
 package com.nazipov.merapet.dto;
 
-public class UserInfo {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import com.nazipov.merapet.utils.Gender;
+
+public class UserInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime dateOfBirth;
+    private Gender gender;
+
+    public UserInfo(
+        String username,
+        String password,
+        String email,
+        LocalDateTime dateOfBirth,
+        Gender gender
+    ) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
     
 }
