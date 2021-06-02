8c8d381d8917a11abc294ae4559a3f2d9a516df0
Niyaz Nazipov <niaz.nazipoff@yandex.ru>
2021-06-02 16:29:50 +0300

    added logger

--- src/main/java/com/nazipov/merapet/controller/UserController.java
+++ src/main/java/com/nazipov/merapet/controller/UserController.java
@@ -7,6 +7,8 @@ import com.nazipov.merapet.entities.MyUser;
 import com.nazipov.merapet.mapper.UserMapper;
 import com.nazipov.merapet.service.UserService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,7 @@ import reactor.core.publisher.Mono;
 public class UserController {
 
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -44,7 +47,7 @@ public class UserController {
         return user
             .map(UserMapper::mapToMyUserFromUserInfo)
             .flatMap(userService::saveUser).onErrorResume(e -> {
-                System.out.println("Error " + e);
+                logger.error(e.toString());
                 return Mono.empty();
             });
     }
--- src/main/java/com/nazipov/merapet/service/impl/UserServiceImpl.java
+++ src/main/java/com/nazipov/merapet/service/impl/UserServiceImpl.java
@@ -1,11 +1,11 @@
 package com.nazipov.merapet.service.impl;
 
-import java.util.Optional;
-
 import com.nazipov.merapet.entities.MyUser;
 import com.nazipov.merapet.service.UserService;
 import com.nazipov.merapet.storage.Storage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 public class UserServiceImpl implements UserService {
 
     private final Storage storage;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     public UserServiceImpl(Storage storage) {
         this.storage = storage;
@@ -22,7 +23,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public Mono<MyUser> saveUser(MyUser user) {
         return Mono.just(storage.saveUser(user))
-            .doOnEach(System.out::println);
+            .doOnEach(str -> logger.info(str.toString()));
     }
     
 }
